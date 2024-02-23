22e6f42356b074e9a198ebcaed9221cca6d066f5
John Francis <john9francis@gmail.com>
2024-02-23 11:03:25 -0700

    Cli done. now I need to compile it.

--- python/cli.py
+++ python/cli.py
@@ -16,28 +16,67 @@ def main():
 
   # create a interface the user can interact with
 
-  c = Chooser()
+  chooser = Chooser()
 
   running = True
   while running:
     # clear console and write prompt
     clear_console()
     print("Here is your list:")
-    print(c.get_word_list())
+
+    print(chooser.get_word_list())
+
     print("enter the following:")
     print("\"q\" to quit")
     print("\"a\" to add to the list")
     print("\"r\" to get a random choice from the list")
+    print("\"c\" to clear the list")
 
 
     # get user input
     user_input = input(">> ")
 
-    print(user_input)
-
     if user_input == "q":
       print("Quitting...")
       running = False
+
+    elif user_input == "a":
+      # add to word list
+      print("Please enter your word, or several words separated by commas.")
+      words = input(">> ")
+      chooser.add_csv_to_word_list(words)
+
+    elif user_input == "r":
+      # print random word
+      print(chooser.get_random_word())
+      print("Press enter to continue...")
+      input(">> ")
+
+    elif user_input == "c":
+      # make sure user wants to clear
+      print("Are you sure you would like to clear the list?")
+      
+      while True:
+        response = input("(y/n)>> ")
+        response = response.lower()
+
+        if response == "y":
+          print("Clearing list...")
+          chooser.clear_word_list()
+          break
+        elif response == "n":
+          print("Not clearing list.")
+          break
+
+        else: 
+          print("Please enter \"y\" or \"n\".")
+
+
+    else:
+      # unknown input
+      print(f"{user_input} is not recognized, please enter q, a, or r.")
+      input("Press enter to continue...")
+    
 
 
 
