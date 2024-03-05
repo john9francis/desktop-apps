3d84856155e192e5c70b86c8649588295c68272a
John Francis <john9francis@gmail.com>
2024-03-04 20:25:44 -0700

    Added a method of user inputs

--- javaScript/cli.js
+++ javaScript/cli.js
@@ -1,9 +1,12 @@
 const Chooser = require('./chooser.js')
 
+const prompt = require('prompt-sync')();
+
 const main = () => {
   const c = new Chooser();
   console.log("Your list:")
   console.log(c.getWordList())
+  const userInput = prompt("enter something: ")
 }
 
 
